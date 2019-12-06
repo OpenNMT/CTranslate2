253a51ff5484e1ee2f41d16381bfac4b764b8031
Guillaume Klein <guillaume.klein@systrangroup.com>
2019-12-06 13:01:34 +0100

    Optimize LayerNorm on CPU

--- src/ops/layer_norm_cpu.cc
+++ src/ops/layer_norm_cpu.cc
@@ -12,21 +12,26 @@ namespace ctranslate2 {
                             const StorageView& gamma,
                             const StorageView& input,
                             StorageView& output) const {
+      const auto* gamma_data = gamma.data<T>();
+      const auto* beta_data = beta.data<T>();
       size_t depth = input.dim(-1);
       size_t batch_size = input.size() / depth;
-      StorageView tmp({batch_size, depth}, input.dtype(), input.device());
       #pragma omp parallel for
       for (long long i = 0; i < static_cast<long long>(batch_size); ++i) {
         const auto* x = input.data<T>() + i * depth;
         auto* y = output.data<T>() + i * depth;
-        auto* t = tmp.data<T>() + i * depth;
-        auto mean = primitives<>::mean(x, depth);
-        primitives<>::sub(mean, x, y, depth);
-        primitives<>::pow(y, t, static_cast<T>(2), depth);
-        auto variance = primitives<>::mean(t, depth);
-        primitives<>::mul(static_cast<T>(1.0 / sqrt(variance + EPSILON)), y, depth);
-        primitives<>::mul(gamma.data<T>(), y, depth);
-        primitives<>::add(beta.data<T>(), y, depth);
+        T mean = 0;  // sum(x)/n
+        T rstd = 0;  // 1/sqrt(var(x)) where var(x) = sum((x-mean)^2)/n = sum(x^2)/n - mean^2
+        for (size_t j = 0; j < depth; ++j) {
+          mean += x[j];
+          rstd += x[j] * x[j];
+        }
+        mean /= depth;
+        rstd = std::max(rstd / depth - mean * mean, static_cast<T>(0));
+        rstd = static_cast<T>(1) / std::sqrt(rstd + EPSILON);
+        for (size_t j = 0; j < depth; ++j) {
+          y[j] = (x[j] - mean) * rstd * gamma_data[j] + beta_data[j];
+        }
       }
     }
 
