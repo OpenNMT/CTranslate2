f34584f56a66f8c8625f6d802155578481fedf2c
Guillaume Klein <guillaume.klein@systrangroup.com>
2019-12-06 15:30:56 +0100

    Add kernel function

--- src/ops/layer_norm_cpu.cc
+++ src/ops/layer_norm_cpu.cc
@@ -7,32 +7,46 @@
 namespace ctranslate2 {
   namespace ops {
 
+    template <typename T>
+    static void layer_norm_kernel(int64_t m,
+                                  int64_t n,
+                                  const T* input,
+                                  const T* gamma,
+                                  const T* beta,
+                                  T* output) {
+      #pragma omp parallel for
+      for (int64_t i = 0; i < m; ++i) {
+        const auto offset = i * n;
+        const T* x = input + offset;
+        T* y = output + offset;
+        T mean = 0;  // sum(x)/n
+        T rstd = 0;  // 1/sqrt(var(x)) where var(x) = sum((x-mean)^2)/n = sum(x^2)/n - mean^2
+        for (int64_t j = 0; j < n; ++j) {
+          mean += x[j];
+          rstd += x[j] * x[j];
+        }
+        mean /= n;
+        rstd = std::max(rstd / n - mean * mean, static_cast<T>(0));
+        rstd = static_cast<T>(1) / std::sqrt(rstd + EPSILON);
+        for (int64_t j = 0; j < n; ++j) {
+          y[j] = (x[j] - mean) * rstd * gamma[j] + beta[j];
+        }
+      }
+    }
+
     template <Device D, typename T>
     void LayerNorm::compute(const StorageView& beta,
                             const StorageView& gamma,
                             const StorageView& input,
                             StorageView& output) const {
-      const auto* gamma_data = gamma.data<T>();
-      const auto* beta_data = beta.data<T>();
-      size_t depth = input.dim(-1);
-      size_t batch_size = input.size() / depth;
-      #pragma omp parallel for
-      for (long long i = 0; i < static_cast<long long>(batch_size); ++i) {
-        const auto* x = input.data<T>() + i * depth;
-        auto* y = output.data<T>() + i * depth;
-        T mean = 0;  // sum(x)/n
-        T rstd = 0;  // 1/sqrt(var(x)) where var(x) = sum((x-mean)^2)/n = sum(x^2)/n - mean^2
-        for (size_t j = 0; j < depth; ++j) {
-          mean += x[j];
-          rstd += x[j] * x[j];
-        }
-        mean /= depth;
-        rstd = std::max(rstd / depth - mean * mean, static_cast<T>(0));
-        rstd = static_cast<T>(1) / std::sqrt(rstd + EPSILON);
-        for (size_t j = 0; j < depth; ++j) {
-          y[j] = (x[j] - mean) * rstd * gamma_data[j] + beta_data[j];
-        }
-      }
+      auto depth = input.dim(-1);
+      auto batch_size = input.size() / depth;
+      layer_norm_kernel(batch_size,
+                        depth,
+                        input.data<T>(),
+                        gamma.data<T>(),
+                        beta.data<T>(),
+                        output.data<T>());
     }
 
 #define DECLARE_IMPL(T)                                                 \
