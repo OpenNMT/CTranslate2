91179d89ce45537da00c690064552e55bc49e480
Guillaume Klein <guillaume.klein@systrangroup.com>
2019-12-06 15:38:09 +0100

    Pass epsilon as typed argument

--- src/ops/layer_norm_cpu.cc
+++ src/ops/layer_norm_cpu.cc
@@ -2,7 +2,7 @@
 
 #include <cmath>
 
-#define EPSILON 0.000001f
+#define EPSILON 1e-5
 
 namespace ctranslate2 {
   namespace ops {
@@ -10,6 +10,7 @@ namespace ctranslate2 {
     template <typename T>
     static void layer_norm_kernel(int64_t m,
                                   int64_t n,
+                                  T eps,
                                   const T* input,
                                   const T* gamma,
                                   const T* beta,
@@ -27,7 +28,7 @@ namespace ctranslate2 {
         }
         mean /= n;
         rstd = std::max(rstd / n - mean * mean, static_cast<T>(0));
-        rstd = static_cast<T>(1) / std::sqrt(rstd + EPSILON);
+        rstd = static_cast<T>(1) / std::sqrt(rstd + eps);
         for (int64_t j = 0; j < n; ++j) {
           y[j] = (x[j] - mean) * rstd * gamma[j] + beta[j];
         }
@@ -43,6 +44,7 @@ namespace ctranslate2 {
       auto batch_size = input.size() / depth;
       layer_norm_kernel(batch_size,
                         depth,
+                        static_cast<T>(EPSILON),
                         input.data<T>(),
                         gamma.data<T>(),
                         beta.data<T>(),
