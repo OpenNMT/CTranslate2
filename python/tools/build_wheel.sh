953038b3ebb964db2148bc44a8c5529a31d4eeb0
Guillaume Klein <guillaume.klein@systrangroup.com>
2020-12-15 16:42:51 +0100

    Fix DNNL lib path

--- python/tools/build_wheel.sh
+++ python/tools/build_wheel.sh
@@ -47,7 +47,7 @@ do
     rm -rf build
 done
 
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$ROOT_DIR/dnnl/lib64
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/intel/oneapi/dnnl/latest/cpu_gomp/lib
 for wheel in dist/*
 do
     auditwheel show $wheel
