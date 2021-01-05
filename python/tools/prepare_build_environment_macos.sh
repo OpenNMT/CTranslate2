5bdd80f3194e8eca51ded01e428ffe9ce463f8fa
Keichi Takahashi <keichi.t@me.com>
2021-01-05 22:48:29 +0900

    Install libomp to help CMake detect OpenMP on macOS

--- python/tools/prepare_build_environment_macos.sh
+++ python/tools/prepare_build_environment_macos.sh
@@ -12,10 +12,14 @@ sudo /Volumes/$(basename $ONEAPI_INSTALLER_URL .dmg)/bootstrapper.app/Contents/M
 # Prefix the install name of libdnnl with @rpath
 sudo install_name_tool -id @rpath/libdnnl.2.dylib /opt/intel/oneapi/dnnl/latest/cpu_iomp/lib/libdnnl.2.0.dylib
 
+# Install LLVM's libomp because Intel's OpenMP runtime included in MKL does
+# not ship with the header file.
+brew install libomp
+
 pip install "cmake==3.18.4"
 
 mkdir build-release && cd build-release
-cmake -DCMAKE_BUILD_TYPE=Release -DLIB_ONLY=ON -DWITH_DNNL=ON -DCMAKE_INSTALL_RPATH_USE_LINK_PATH=ON ..
+cmake -DCMAKE_BUILD_TYPE=Release -DLIB_ONLY=ON -DWITH_DNNL=ON -DCMAKE_INSTALL_RPATH_USE_LINK_PATH=ON -DCMAKE_CXX_FLAGS="-Wno-unused-command-line-argument" ..
 make -j$(sysctl -n hw.physicalcpu_max) install
 cd ..
 rm -r build-release
