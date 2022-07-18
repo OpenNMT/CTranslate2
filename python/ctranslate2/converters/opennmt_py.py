8ab0f1edc0a23f3649e67805366824df600ccfb1
Valentin Berkes <16121857+funboarder13920@users.noreply.github.com>
2022-07-18 07:58:12 +0000

    fix opennmt-py converter relative position in LM
    https://github.com/OpenNMT/CTranslate2/pull/844

--- python/ctranslate2/converters/opennmt_py.py
+++ python/ctranslate2/converters/opennmt_py.py
@@ -92,7 +92,6 @@ def _get_model_spec_seq2seq(
 
 def _get_model_spec_lm(opt, variables, src_vocabs, tgt_vocabs, num_source_embeddings):
     """Creates a model specification from the model options."""
-    with_relative_position = getattr(opt, "max_relative_positions", 0) > 0
     activation_fn = getattr(opt, "pos_ffn_activation_fn", "relu")
     num_heads = getattr(opt, "heads", 8)
 
@@ -105,7 +104,6 @@ def _get_model_spec_lm(opt, variables, src_vocabs, tgt_vocabs, num_source_embedd
     set_transformer_decoder(
         model_spec.decoder,
         variables,
-        with_relative_position,
         with_encoder_attention=False,
     )
 
