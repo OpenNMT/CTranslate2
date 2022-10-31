680345952e00ee74730268475edd2d9876cfea1d
Guillaume Klein <guillaume.klein@systrangroup.com>
2022-10-31 09:39:24 +0100

    Install cuDNN devel package

--- python/tools/prepare_build_environment_linux.sh
+++ python/tools/prepare_build_environment_linux.sh
@@ -28,7 +28,7 @@ else
         cuda-nvcc-11-2-11.2.152-1 \
         cuda-cudart-devel-11-2-11.2.152-1 \
         libcurand-devel-11-2-10.2.3.152-1 \
-        libcudnn8-8.1.1.33-1.cuda11.2 \
+        libcudnn8-devel-8.1.1.33-1.cuda11.2 \
         libcublas-devel-11-2-11.4.1.1043-1
     ln -s cuda-11.2 /usr/local/cuda
 
