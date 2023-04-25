de517ca256b59c1940d7d9c62995b181a7c9864b
Guillaume Klein <guillaume.klein@systrangroup.com>
2023-04-25 10:08:50 +0200

    Always read OMP_NUM_THREADS

--- src/utils.cc
+++ src/utils.cc
@@ -79,13 +79,12 @@ namespace ctranslate2 {
   }
 
   void set_num_threads(size_t num_threads) {
-#ifdef _OPENMP
     if (num_threads == 0)
       num_threads = read_int_from_env("OMP_NUM_THREADS", get_default_num_threads());
+
+#ifdef _OPENMP
     omp_set_num_threads(num_threads);
 #else
-    if (num_threads == 0)
-      num_threads = get_default_num_threads();
     cpu::set_num_threads(num_threads);
 #endif
 
