e23c9f8a6db5f229a934b3fea7e8fa88a261a57b
Guillaume Klein <guillaume.klein@systrangroup.com>
2023-07-27 11:02:18 +0200

    Accept left offsets in the masked softmax operator

--- src/ops/softmax.cc
+++ src/ops/softmax.cc
@@ -14,28 +14,38 @@ namespace ctranslate2 {
     }
 
     void SoftMax::operator()(StorageView& x) const {
-      operator()(x, nullptr, x);
+      operator()(x, nullptr, nullptr, x);
     }
 
     void SoftMax::operator()(const StorageView& x, StorageView& y) const {
-      operator()(x, nullptr, y);
+      operator()(x, nullptr, nullptr, y);
     }
 
     void SoftMax::operator()(const StorageView& x, const StorageView& lengths, StorageView& y) const {
-      operator()(x, &lengths, y);
+      operator()(x, &lengths, nullptr, y);
     }
 
-    void SoftMax::operator()(const StorageView& x, const StorageView* lengths, StorageView& y) const {
+    void SoftMax::operator()(const StorageView& x,
+                             const StorageView& lengths,
+                             const StorageView& offsets,
+                             StorageView& y) const {
+      operator()(x, &lengths, &offsets, y);
+    }
+
+    void SoftMax::operator()(const StorageView& x,
+                             const StorageView* lengths,
+                             const StorageView* offsets,
+                             StorageView& y) const {
       PROFILE(_log ? "LogSoftMax" : "SoftMax");
       y.resize_as(x);
 
       const dim_t depth = x.dim(-1);
+      const dim_t batch_size = x.size() / depth;
 
       if (depth == 0)
         return;
 
       if (lengths) {
-        const dim_t batch_size = x.size() / depth;
         if (lengths->size() != batch_size)
           throw std::invalid_argument("Length mask has size "
                                       + std::to_string(lengths->size())
@@ -43,7 +53,16 @@ namespace ctranslate2 {
                                       + std::to_string(batch_size));
       }
 
-      DEVICE_AND_FLOAT_DISPATCH("SoftMax", x.device(), x.dtype(), (compute<D, T>(x, lengths, y)));
+      if (offsets) {
+        if (offsets->size() != batch_size)
+          throw std::invalid_argument("Offsets input has size "
+                                      + std::to_string(offsets->size())
+                                      + " which is different than the current batch size "
+                                      + std::to_string(batch_size));
+      }
+
+      DEVICE_AND_FLOAT_DISPATCH("SoftMax", x.device(), x.dtype(),
+                                (compute<D, T>(x, lengths, offsets, y)));
     }
 
   }
