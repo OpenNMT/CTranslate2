5f347291c8827b2208db512f7350b6edeb6882b5
thucpham <minhthuc.pham@systrangroup.com>
2023-12-21 00:15:55 +0100

    add silence model in cudnn installer

--- python/tools/prepare_build_environment_windows.sh
+++ python/tools/prepare_build_environment_windows.sh
@@ -10,7 +10,7 @@ rm cuda.exe
 
 CUDNN_ROOT="C:\Program Files\NVIDIA\CUDNN\v8.8"
 curl -L -nv -o cudnn.exe https://developer.download.nvidia.com/compute/redist/cudnn/v8.8.0/local_installers/12.0/cudnn_8.8.0.121_windows.exe
-./cudnn.exe
+./cudnn.exe -s
 cp -r "$CUDNN_ROOT/*" "$CUDA_ROOT"
 rm cudnn.exe
 
