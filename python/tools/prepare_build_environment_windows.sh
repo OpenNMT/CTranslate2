61590af11a6cc7c41e260b575021327a421ad1d9
thucpham <minhthuc.pham@systrangroup.com>
2023-12-21 00:26:06 +0100

    try new cudnn root

--- python/tools/prepare_build_environment_windows.sh
+++ python/tools/prepare_build_environment_windows.sh
@@ -8,7 +8,7 @@ curl -L -nv -o cuda.exe https://developer.download.nvidia.com/compute/cuda/12.2.
 ./cuda.exe -s nvcc_12.2 cudart_12.2 cublas_dev_12.2 curand_dev_12.2
 rm cuda.exe
 
-CUDNN_ROOT="C:\Program Files\NVIDIA\CUDNN\v8.8"
+CUDNN_ROOT="C:/Program Files/NVIDIA/CUDNN/v8.8"
 curl -L -nv -o cudnn.exe https://developer.download.nvidia.com/compute/redist/cudnn/v8.8.0/local_installers/12.0/cudnn_8.8.0.121_windows.exe
 ./cudnn.exe -s
 cp -r "$CUDNN_ROOT/*" "$CUDA_ROOT"
