3860e937537b218e51cc7fb25c233d1a1290c57a
minhthuc2502 <46375464+minhthuc2502@users.noreply.github.com>
2024-02-15 14:03:22 +0000

    Deploying to gh-pages from @ OpenNMT/CTranslate2@61492e00246b5e3818b56fb27d570220c87b6194 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '3.24.0',
+    VERSION: '4.0.0',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
