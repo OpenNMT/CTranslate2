ddd3f80440375c5d9af311e6e75dc830bf75b9ce
minhthuc <minhthuc2502@gmail.com>
2024-08-13 09:10:13 +0200

    fix black

--- python/tests/test_translator.py
+++ python/tests/test_translator.py
@@ -111,9 +111,12 @@ def test_batch_translation(max_batch_size):
     assert output[0].scores[0] < 0
     assert not output[0].attention
 
-    expected_repr = "TranslationResult(hypotheses=%s, scores=%s, attention=[], log_probs=[])" % (
-        output[0].hypotheses,
-        output[0].scores,
+    expected_repr = (
+        "TranslationResult(hypotheses=%s, scores=%s, attention=[], log_probs=[])"
+        % (
+            output[0].hypotheses,
+            output[0].scores,
+        )
     )
     assert repr(output[0]) == expected_repr
 
