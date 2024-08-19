8ba828c0cf3d72e93ec675cd2e472b64b8c55b64
homink <homin.kwon@gmail.com>
2024-08-19 05:21:52 -0700

    Wav2Vec2 upgrade with Conv1D options (#1758)
    * Wav2Vec2 upgrade with Conv1D options
    
    * refining scripts
    
    * refining script again
    
    * fix the formats
    
    * fix the isort format
    
    * refining the library
    
    * update based on the suggestions
    
    * update the variable name
    
    * adding unk_token removal for the Python testing
    
    * adding whitespace
    
    * update Python format
    
    * update variables
    
    * update variables
    
    * update variables
    
    * update variables
    
    ---------
    
    Co-authored-by: hkwon <homin.kwon@sri.com>

--- python/ctranslate2/specs/wav2vec2_spec.py
+++ python/ctranslate2/specs/wav2vec2_spec.py
@@ -13,10 +13,9 @@ class Wav2Vec2Config(model_spec.ModelConfig):
 
 
 class Wav2Vec2Spec(model_spec.LanguageModelSpec):
-    def __init__(self, num_layers, num_heads):
+    def __init__(self, feat_layers, num_layers, num_heads):
         super().__init__()
-        self.encoder = Wav2Vec2EncoderSpec(num_layers, num_heads)
-        self.lm_head = common_spec.LinearSpec()
+        self.encoder = Wav2Vec2EncoderSpec(feat_layers, num_layers, num_heads)
 
     @property
     def name(self):
@@ -30,14 +29,30 @@ class Wav2Vec2Spec(model_spec.LanguageModelSpec):
         return Wav2Vec2Config()
 
     def get_vocabulary_size(self):
-        return self.lm_head.weight.shape[0]
+        return self.encoder.lm_head.weight.shape[0]
+
+
+class Wav2Vec2LayerNormConvLayer(model_spec.LayerSpec):
+    def __init__(self):
+        self.conv = common_spec.Conv1DSpec()
+        self.layer_norm = common_spec.LayerNormSpec()
+
+
+class Wav2Vec2PosEmbedConvLayer(model_spec.LayerSpec):
+    def __init__(self):
+        self.conv = common_spec.Conv1DSpec()
 
 
 class Wav2Vec2EncoderSpec(model_spec.LayerSpec):
-    def __init__(self, num_layers, num_heads):
+    def __init__(self, feat_layers, num_layers, num_heads):
         self.num_heads = np.dtype("int16").type(num_heads)
-        # wav2vec2.encoder modules except pos_conv_embed due to groups=16 being not supported
+        self.feat_layer0 = Wav2Vec2LayerNormConvLayer()
+        self.feat_layer = [Wav2Vec2LayerNormConvLayer() for i in range(feat_layers - 1)]
+        self.fp_layer_norm = common_spec.LayerNormSpec()
+        self.fp_projection = common_spec.LinearSpec()
+        self.pos_conv_embed = Wav2Vec2PosEmbedConvLayer()
         self.layer_norm = common_spec.LayerNormSpec()
         self.layer = [
             transformer_spec.TransformerEncoderLayerSpec() for _ in range(num_layers)
         ]
+        self.lm_head = common_spec.LinearSpec()
--- src/layers/wav2vec2.cc
+++ src/layers/wav2vec2.cc
@@ -3,14 +3,66 @@
 
 namespace ctranslate2 {
   namespace layers {
+
+    Wav2Vec2LayerNormConvLayer::Wav2Vec2LayerNormConvLayer(const models::Model& model,
+                                                           const std::string& scope,
+                                                           dim_t stride,
+                                                           dim_t padding)
+      : _stride(stride)
+      , _padding(padding)
+      , _conv(model, scope + "/conv", _stride, _padding)
+      , _transpose({0, 2, 1})
+      , _output_norm(model, scope + "/layer_norm") {
+    }
+
+    void Wav2Vec2LayerNormConvLayer::operator()(const StorageView& input, StorageView& output) const{
+      PROFILE("Wav2Vec2LayerNormConvLayer");
+
+      StorageView buffer(input.dtype(), input.device());
+      buffer = std::move(input);
+      _conv(buffer, output);
+      _transpose(output, buffer);
+      _output_norm(buffer, output);
+      _transpose(output, buffer);
+      _gelu(buffer, output);
+    }
+
+    Wav2Vec2PosConvLayer::Wav2Vec2PosConvLayer(const models::Model& model, const std::string& scope)
+      : _conv(model, scope + "/conv", /*stride=*/1, /*padding=*/64, /*dilation*/1, /*groups*/16)
+      , _transpose({0, 2, 1}) {
+    }
+
+    void Wav2Vec2PosConvLayer::operator()(const StorageView& input, StorageView& output) const{
+      PROFILE("Wav2Vec2PosConvLayer");
+
+      StorageView buffer(input.dtype(), input.device());
+      StorageView buffer2(input.dtype(), input.device());
+      _transpose(input, buffer);
+      _conv(buffer, buffer2);
+      ops::Split(2, {buffer.dim(2), 1})(buffer2, buffer, output);
+      _gelu(buffer, buffer);
+      _transpose(buffer, buffer2);
+      ops::Add()(input, buffer2, output);
+    }
+
     Wav2Vec2Encoder::Wav2Vec2Encoder(const models::Model& model, const std::string& scope)
-      : _num_heads(model.get_attribute_with_default<int32_t>(scope + "/num_heads", 8))
+      : _feat_layer0(model, scope + "/feat_layer0", /*stride=*/5, /*padding=*/0)
+      , _feat_layers(build_layers_list<const Wav2Vec2LayerNormConvLayer>(model,
+                                                                         scope + "/feat_layer",
+                                                                         /*stride=*/2,
+                                                                         /*padding=*/0))
+      , _fp_norm(model, scope + "/fp_layer_norm")
+      , _fp_ff(model, scope + "/fp_projection", nullptr, true)
+      , _pos_conv_embed(model, scope + "/pos_conv_embed")
+      , _num_heads(model.get_attribute_with_default<int32_t>(scope + "/num_heads", 8))
+      , _transpose({0, 2, 1})
       , _layers(build_layers_list<const TransformerEncoderLayer>(model,
                                                                  scope + "/layer",
                                                                  _num_heads,
                                                                  /*pre_norm=*/true,
                                                                  ops::ActivationType::GELU))
       , _output_norm(model, scope + "/layer_norm")
+      , _lm_head(model, scope + "/lm_head", nullptr, true)
     {
     }
 
@@ -18,40 +70,37 @@ namespace ctranslate2 {
       PROFILE("Wav2Vec2Encoder");
 
       // SAD in front-end handles the input length
-      //const dim_t expected_depth = 1024;
-      //const dim_t expected_time = 406;
-
       if (features.rank() != 3)
         throw std::invalid_argument("Expected input features to have 3 dimensions, but got "
                                     + std::to_string(features.rank())
                                     + " dimension(s) instead");
-      /* //may need to limit the input lenght
-      if (features.dim(1) != expected_depth || features.dim(2) != expected_time)
-        throw std::invalid_argument("Invalid input features shape: expected an input with shape ("
-                                    + std::to_string(features.dim(0))
-                                    + ", "
-                                    + std::to_string(expected_depth)
-                                    + ", "
-                                    + std::to_string(expected_time)
-                                    + "), but got an input with shape ("
-                                    + std::to_string(features.dim(0))
-                                    + ", "
-                                    + std::to_string(features.dim(1))
-                                    + ", "
-                                    + std::to_string(features.dim(2))
-                                    + ") instead;; _conv1.output_size() "
-                                    + std::to_string(_conv1.output_size()));
-                                    //+ ") instead");
-      */
 
-      StorageView input(output_type(), features.device());
-      input = features;
-      for (const auto& layer : _layers) {
-        (*layer)(input, nullptr, output);
-        input = std::move(output);
+      // Wav2Vec2FeatureExtractor------------------------------------
+      StorageView feat_buffer(features.dtype(), features.device());
+      StorageView feat_buffer2(features.dtype(), features.device());
+      feat_buffer = std::move(features);
+      _feat_layer0(feat_buffer, output);
+      feat_buffer = std::move(output);
+      for (dim_t l = 0; l < _feat_layers.size(); l++) {
+        (*_feat_layers[l])(feat_buffer, output);
+        if (l < _feat_layers.size() - 1 ) {
+          feat_buffer = std::move(output);
+        }
       }
+      _transpose(output, feat_buffer);
+      // Wav2Vec2FeatureProjection-----------------------------------
+      _fp_norm(feat_buffer, output);
+      _fp_ff(output, feat_buffer);
+      // Wav2Vec2PositionalConvEmbedding-----------------------------
+      _pos_conv_embed(feat_buffer, feat_buffer2);
+      // Wav2Vec2EncoderLayerStableLayerNorm-------------------------
+      for (const auto& layer : _layers) {
+        (*layer)(feat_buffer2, nullptr, feat_buffer);
+        feat_buffer2 = std::move(feat_buffer);
+      }
+      _output_norm(feat_buffer2, feat_buffer);
 
-      _output_norm(input, output);
+      _lm_head(feat_buffer, output);
     }
 
   }
