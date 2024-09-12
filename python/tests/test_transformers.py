3bd52197e6328444a17114a940f035834354582b
hkwon <homin.kwon@sri.com>
2024-09-12 08:40:07 -0700

    upgrade transformers 4.41 for python test

--- python/tests/test_transformers.py
+++ python/tests/test_transformers.py
@@ -1028,16 +1028,6 @@ class TestWav2Vec2:
 class TestWav2Vec2Bert:
     @classmethod
     def teardown_class(cls):
-        import transformers
-
-        required_version = "4.41.0"
-        current_version = transformers.__version__
-        if current_version < required_version:
-            pytest.skip(
-                f"Transformers version must be >= {required_version},"
-                " but found {current_version}"
-            )
-
         clear_transformers_cache_in_ci()
 
     @test_utils.only_on_linux
@@ -1063,14 +1053,6 @@ class TestWav2Vec2Bert:
     ):
         import torch
         import transformers
-
-        required_version = "4.41.0"
-        current_version = transformers.__version__
-        if current_version < required_version:
-            pytest.skip(
-                f"Transformers version must be >= {required_version},"
-                " but found {current_version}"
-            )
 
         converter = ctranslate2.converters.TransformersConverter(
             model_name, load_as_float16="int8"
