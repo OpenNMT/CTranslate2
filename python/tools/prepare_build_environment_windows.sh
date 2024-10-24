aab2953e4bd1262d8ed0e31d74dc954027d2b1bd
MahmoudAshraf97 <hassouna97.ma@gmail.com>
2024-10-24 15:33:32 +0300

    downgrade cudnn

--- python/tools/prepare_build_environment_windows.sh
+++ python/tools/prepare_build_environment_windows.sh
@@ -8,8 +8,8 @@ curl -L -nv -o cuda.exe https://developer.download.nvidia.com/compute/cuda/12.2.
 ./cuda.exe -s nvcc_12.2 cudart_12.2 cublas_dev_12.2 curand_dev_12.2
 rm cuda.exe
 
-CUDNN_ROOT="C:/Program Files/NVIDIA/CUDNN/v9.1"
-curl -L -nv -o cudnn.exe https://developer.download.nvidia.com/compute/cudnn/9.1.0/local_installers/cudnn_9.1.0_windows.exe
+CUDNN_ROOT="C:/Program Files/NVIDIA/CUDNN/v9.0"
+curl -L -nv -o cudnn.exe https://developer.download.nvidia.com/compute/cudnn/9.0.0/local_installers/cudnn_9.0.0_windows.exe
 ./cudnn.exe -s
 sleep 10
 # Remove 11.8 folders
@@ -18,14 +18,14 @@ rm -rf "$CUDNN_ROOT/lib/11.8"
 rm -rf "$CUDNN_ROOT/include/11.8"
 
 # Move contents of 12.4 to parent directories
-mv "$CUDNN_ROOT/bin/12.4/"* "$CUDNN_ROOT/bin/"
-mv "$CUDNN_ROOT/lib/12.4/"* "$CUDNN_ROOT/lib/"
-mv "$CUDNN_ROOT/include/12.4/"* "$CUDNN_ROOT/include/"
+mv "$CUDNN_ROOT/bin/12.3/"* "$CUDNN_ROOT/bin/"
+mv "$CUDNN_ROOT/lib/12.3/"* "$CUDNN_ROOT/lib/"
+mv "$CUDNN_ROOT/include/12.3/"* "$CUDNN_ROOT/include/"
 
 # Remove empty 12.4 folders
-rmdir "$CUDNN_ROOT/bin/12.4"
-rmdir "$CUDNN_ROOT/lib/12.4"
-rmdir "$CUDNN_ROOT/include/12.4"
+rmdir "$CUDNN_ROOT/bin/12.3"
+rmdir "$CUDNN_ROOT/lib/12.3"
+rmdir "$CUDNN_ROOT/include/12.3"
 cp -r "$CUDNN_ROOT"/* "$CUDA_ROOT"
 rm cudnn.exe
 
