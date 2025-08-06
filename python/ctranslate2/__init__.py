157c75d9f4cc97916ef8ea6fdab80c60f011da78
Thawan Barroso <thawancomt@gmail.com>
2025-08-06 16:26:54 +0100

    fix: update package_dir assignment to use resources.files for compatibility and removing the dependence for a deprecated package (pkg_resources)

--- python/ctranslate2/__init__.py
+++ python/ctranslate2/__init__.py
@@ -6,9 +6,12 @@ if sys.platform == "win32":
     import os
 
     import pkg_resources
+    from importlib import resources
 
     module_name = sys.modules[__name__].__name__
-    package_dir = pkg_resources.resource_filename(module_name, "")
+    # Fixed the pkg_resources depreciation
+    package_dir = str(resources.files(module_name))
+    
 
     add_dll_directory = getattr(os, "add_dll_directory", None)
     if add_dll_directory is not None:
