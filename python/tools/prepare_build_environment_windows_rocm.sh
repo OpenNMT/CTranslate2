e7a472746f0cae632de4ef73749c13474cedd07a
sssshhhhhh <193317444+sssshhhhhh@users.noreply.github.com>
2026-01-30 10:06:33 +1100

    Linux whls

--- python/tools/prepare_build_environment_windows_rocm.sh
+++ python/tools/prepare_build_environment_windows_rocm.sh
@@ -27,17 +27,18 @@ curl --netrc-optional -L -nv -o webimage.exe https://registrationcenter-download
 rm webimage.exe
 ./webimage_extracted/bootstrapper.exe -s --action install --components="intel.oneapi.win.mkl.devel" --eula=accept -p=NEED_VS2017_INTEGRATION=0 -p=NEED_VS2019_INTEGRATION=0 --log-dir=.
 
+NPROC=$(nproc)
 ONEDNN_VERSION=3.10.2
 curl --netrc-optional -L -O https://github.com/uxlfoundation/oneDNN/archive/refs/tags/v${ONEDNN_VERSION}.tar.gz
 tar xf *.tar.gz && rm *.tar.gz
 cd oneDNN-*
 cmake -DCMAKE_BUILD_TYPE=Release -DONEDNN_LIBRARY_TYPE=STATIC -DONEDNN_BUILD_EXAMPLES=OFF -DONEDNN_BUILD_TESTS=OFF -DONEDNN_ENABLE_WORKLOAD=INFERENCE -DONEDNN_ENABLE_PRIMITIVE="CONVOLUTION;REORDER" -DONEDNN_BUILD_GRAPH=OFF .
-cmake --build . --config Release --target install --parallel 6
+cmake --build . --config Release --target install --parallel $NPROC
 cd ..
 rm -r oneDNN-*
 
 cmake -GNinja -DCMAKE_BUILD_TYPE=Release -S . -B build -DCMAKE_CXX_FLAGS="-Wno-deprecated-literal-operator" -DCMAKE_HIP_FLAGS="-Wno-deprecated-literal-operator" -DCMAKE_INSTALL_PREFIX=$CTRANSLATE2_ROOT -DCMAKE_PREFIX_PATH="C:/Program Files (x86)/Intel/oneAPI/compiler/latest/lib;C:/Program Files (x86)/oneDNN" -DBUILD_CLI=OFF -DWITH_DNNL=ON -DWITH_HIP=ON -DCMAKE_HIP_ARCHITECTURES="$PYTORCH_ROCM_ARCH"
-cmake --build build --config Release --target install --parallel 6 --verbose
+cmake --build build --config Release --target install --parallel $NPROC --verbose
 rm -r build
 
 cp README.md python/
